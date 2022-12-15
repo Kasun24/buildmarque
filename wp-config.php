aa6203d8d90c0362aaa7aa5106e0d26f2acd6c56
Kasun24 <kasun961224@gmail.com>
2022-12-15 06:58:37 +0530

    db done

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'buildmarque' );
+define( 'DB_NAME', 'yogeedev_buildmarque' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'yogeedev_buildmarque' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'x,1#9D4DoVUw' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
