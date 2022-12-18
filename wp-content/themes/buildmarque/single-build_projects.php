e4a4158a25fd987d60d8a86454c835ffafae8903
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-18 09:29:49 +0530

    test

--- wp-content/themes/buildmarque/single-build_projects.php
+++ wp-content/themes/buildmarque/single-build_projects.php
@@ -24,17 +24,31 @@ get_header();  ?>
           <?php endforeach; ?>
         </div>
       <?php endif; ?>
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Popup image</button>
+      <!-- test -->
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
 
-      <div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
             <div class="modal-body">
-              <img src="//placehold.it/1000x600" class="img-responsive">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
             </div>
           </div>
         </div>
       </div>
+      <!-- test -->
     </div>
 
     <div class="back_btn text-center">
