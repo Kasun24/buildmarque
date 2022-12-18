4b8926e54fa1975b316f849190cbad63d8af4b7e
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-18 10:15:39 +0530

    test

--- wp-content/themes/buildmarque/single-build_projects.php
+++ wp-content/themes/buildmarque/single-build_projects.php
@@ -38,15 +38,10 @@ get_header();  ?>
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
                    <img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                   </div>
                 </div>
               </div>
