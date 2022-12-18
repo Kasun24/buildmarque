e35bd7c2bc5c83cb340d500ae961dec65c29b26f
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-12-18 09:10:31 +0530

    test

--- wp-content/themes/buildmarque/page-build_projects.php
+++ wp-content/themes/buildmarque/page-build_projects.php
@@ -13,23 +13,7 @@ get_header();  ?>
       <h2 class="text-second mb-5">Projects</h2>
     </div>
     <div class="content pt-2 pb-5">
-      <?php
-      $args = array(
-        'post_type' => 'build_projects',
-        'orderby' => 'name',
-        'order'   => 'ASC'
-      );
 
-      $cats = get_categories($args);
-
-      foreach ($cats as $cat) {
-      ?>
-        <a href="<?php echo get_category_link($cat->term_id) ?>">
-          <?php echo $cat->name; ?>
-        </a>
-      <?php
-      }
-      ?>
       <div class="pr0_tab text-center">
         <button class="pr0_tablinks bg-white fw-bold text-uppercase px-4" onclick="openCity(event, 'cat_1')">
           All
@@ -51,15 +35,34 @@ get_header();  ?>
       <div id="cat_1" class="pr0_tabcontent" style="display: block">
         <div class="sing_cat_sec">
           <div class="row">
-            <div class="col-sm-4 mt-3">
-              <a href="<?php echo get_home_url(); ?>/build_projects/new-build-bergholt/">
-                <div class="proj" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/img/h1.jpg)">
-                  <div class="proj_cap text-center text-light text-uppercase">
-                    new build bergholt
-                  </div>
+            <?php
+            $args = array(
+              'post_type' => 'build_projects',
+              'category_name' => 'internal-refurbs'
+            );
+
+            $post_query = new WP_Query($args);
+
+            if ($post_query->have_posts()) {
+              while ($post_query->have_posts()) {
+                $post_query->the_post();
+                $featured_img_url = wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail');
+                $cats = get_the_category($post->ID);
+            ?>
+                <div class="col-sm-4 mt-3">
+                  <a href="<?php echo get_permalink($post->ID); ?>">
+                    <div class="proj" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/img/h1.jpg)">
+                    <!-- <img src="<?php echo $featured_img_url; ?>" alt=""> -->
+                      <div class="proj_cap text-center text-light text-uppercase">
+                        <?php the_title(); ?>
+                      </div>
+                    </div>
+                  </a>
                 </div>
-              </a>
-            </div>
+            <?php
+              }
+            }
+            ?>
             <div class="col-sm-4 mt-3">
               <a href="#">
                 <div class="proj" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/img/h2.jpg)">
@@ -292,7 +295,7 @@ get_header();  ?>
 </div>
 
 <!-- test -->
-<?php
+<!-- <?php
 $args = array(
   'post_type' => 'build_projects',
   'category_name' => 'kitchens-bathrooms'
@@ -325,7 +328,7 @@ if ($post_query->have_posts()) {
 <?php
   }
 }
-?>
+?> -->
 <!-- test -->
 
 <?php get_footer(); ?>
